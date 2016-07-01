7f3eada8b43f262bd88b7bb31e03e48d2e969a3b
luenghw <1126181898@qq.com>
2016-07-01 13:51:38 +0800

    Update and rename ListViewUtils.java to ViewUtils.java

---
+++ ViewUtils.java
@@ -0,0 +1,48 @@
+
+/**
+ * Created by luenghw on 2016/4/11.
+ * reset the ListView or GridView Height when you use it in ScrollView
+ */
+public class ListViewUtils {
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0); // calculate the item height
+            totalHeight += listItem.getMeasuredHeight(); //  calculate the item total height
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight
+                + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        // get the item divider height
+        // get total height of ListView and reset params
+        listView.setLayoutParams(params);
+       }
+       
+        public static void setGridViewHeightBasedOnChildren(GridView gridView) {
+        // 获取ListView对应的Adapter
+        ListAdapter listAdapter = gridView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) { // listAdapter.getCount()返回数据项的数目
+            View listItem = listAdapter.getView(i, null, gridView);
+            listItem.measure(0, 0); // 计算子项View 的宽高
+            totalHeight += listItem.getMeasuredHeight(); // 统计所有子项的总高度
+        }
+        ViewGroup.LayoutParams params = gridView.getLayoutParams();
+        params.height = totalHeight
+                + (gridView.getHorizontalSpacing() * (listAdapter.getCount() - 1));
+        // listView.getDividerHeight()获取子项间分隔符占用的高度
+        // params.height最后得到整个ListView完整显示需要的高度
+        gridView.setLayoutParams(params);
+    }
+
+
+}
